5a29c9701feffcd9440c737a93e506995ea4482b
AnhCBT <cungbuituananh@gmail.com>
2025-06-12 16:26:22 +0700

    handle navbar
    update logic render routes

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,48 +1,127 @@
-import { Link } from "react-router-dom";
-import viteLogo from "/vite.svg";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../assets/imgs/Logo.png";
 import { LIST_ROUTES } from "../../routes/routes";
-import { Button } from "antd";
 import { useTranslation } from "react-i18next";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { useState } from "react";
 
 function Navbar() {
-  const { i18n } = useTranslation();
+  const { t } = useTranslation("menu");
+  const [openDropdown, setOpenDropdown] = useState<string | null>(null);
 
   return (
-    <nav className=" bg-white shadow px-6 py-3  mx-auto">
-      <div className="flex items-center justify-between max-w-7xl mx-auto">
-        <div className="flex items-center gap-2">
-          <img src={viteLogo} alt="Logo" className="h-8 w-8" />
-          <span className="font-bold text-xl">MyApp</span>
-        </div>
-        <ul className="flex gap-6 items-center">
+    <nav
+      className=" bg-white shadow px-6 mx-auto"
+      style={{ boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" }}
+    >
+      <div className="flex items-center justify-between max-w-10xl mx-auto">
+        <button
+          className="flex items-center gap-2 focus:outline-none"
+          onClick={() => {
+            console.log("./");
+          }}
+          aria-label={t("title")}
+          type="button"
+        >
+          <img src={logo} alt="Logo" className="w-35" />
+          <span className="text-[#2F5597]">{t("title")}</span>
+        </button>
+        <ul className="flex gap-6 items-center text-[#4F4F4F] hidden md:flex">
           {LIST_ROUTES.map((route) => (
-            <li key={route.name}>
-              <Link to={route.url} className="hover:text-blue-600">
-                {route.name}
-              </Link>
+            <li
+              key={route.name}
+              className="relative py-4"
+              onMouseEnter={() => setOpenDropdown(route.name)}
+              onMouseLeave={() => setOpenDropdown(null)}
+              onFocus={() => setOpenDropdown(route.name)}
+              onBlur={(e) => {
+                if (!e.currentTarget.contains(e.relatedTarget as Node)) {
+                  setOpenDropdown(null);
+                }
+              }}
+              tabIndex={0} // Make the li focusable for keyboard users
+            >
+              {route.children ? (
+                <button
+                  type="button"
+                  className="hover:text-blue-600 flex items-center gap-2 focus:outline-none group"
+                  onClick={() =>
+                    setOpenDropdown(
+                      openDropdown === route.name ? null : route.name
+                    )
+                  }
+                  aria-haspopup="true"
+                  aria-expanded={openDropdown === route.name}
+                >
+                  <span>{route.icon || null}</span>
+                  <span>{t(route.name)}</span>
+                  <IoMdArrowDropdown className="transition-colors group-hover:text-[#ED7D31]" />
+                </button>
+              ) : (
+                <Link
+                  to={route.url}
+                  className="hover:text-blue-600 flex items-center gap-2"
+                >
+                  <span>{route.icon || null}</span>
+                  <span>{t(route.name)}</span>
+                </Link>
+              )}
+              {route.children && (
+                <ul
+                  className={`absolute left-0 mt-2 min-w-[150px] bg-white shadow-lg rounded z-10 ${
+                    openDropdown === route.name ? "block" : "hidden"
+                  }`}
+                  style={{
+                    border: "1px solid rgba(229, 229, 229, 1)",
+
+                    borderRadius: "10px",
+                  }}
+                >
+                  {route.children.map((child, index) => {
+                    const isOnlyOne = route.children.length === 1;
+                    const isLast =
+                      index === route.children.length - 1 && !isOnlyOne;
+                    const isFirst = index === 0 && !isOnlyOne;
+
+                    const styleBorder = isFirst
+                      ? { borderRadius: "10px 10px 0 0" }
+                      : isLast
+                      ? { borderRadius: "0 0 10px 10px" }
+                      : isOnlyOne
+                      ? { borderRadius: "10px" }
+                      : {};
+
+                    return (
+                      <li key={child.name}>
+                        {"url" in child ? (
+                          <Link
+                            to={child.url}
+                            className="flex items-center gap-2 block px-4 py-2 hover:bg-[#2F5597] hover:text-[#FFF] text-gray-700"
+                            tabIndex={0}
+                            style={styleBorder}
+                          >
+                            <span>{child.icon || null}</span>
+                            {t(child.name)}
+                          </Link>
+                        ) : (
+                          <button
+                            type="button"
+                            onClick={child.event}
+                            className="flex items-center gap-2 block px-4 py-2 hover:bg-[#2F5597] hover:text-[#FFF] text-gray-700 w-full text-left"
+                            tabIndex={0}
+                            style={styleBorder}
+                          >
+                            <span>{child.icon || null}</span>
+                            {t(child.name)}
+                          </button>
+                        )}
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
             </li>
           ))}
-          <li>
-            <Button
-              type="primary"
-              style={{ width: 60, textAlign: "center", padding: 0 }}
-              onClick={() => {
-                console.log("Change language to", i18n.language);
-                i18n.changeLanguage(i18n.language === "en" ? "vi" : "en");
-              }}
-              onMouseEnter={(e) => {
-                const btn = e.currentTarget;
-                btn.dataset.prevText = btn.innerText;
-                btn.innerText = i18n.language === "en" ? "VIE" : "ENG";
-              }}
-              onMouseLeave={(e) => {
-                const btn = e.currentTarget;
-                btn.innerText = btn.dataset.prevText || btn.innerText;
-              }}
-            >
-              {i18n.language === "en" ? "ENG" : "VIE"}
-            </Button>
-          </li>
         </ul>
       </div>
     </nav>
