58a06d94c7be32e57a0852c51465657dc6953dec
anhcbt_pc <cungbuituananh@gmail.com>
2025-06-18 22:36:41 +0700

    update footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { LIST_ROUTES, renderRoutes } from "./routes/routes";
+import Footer from "./components/Navbar/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>{renderRoutes(LIST_ROUTES)}</Routes>
+      <Footer />
     </>
   );
 }
